ac461b9ea2ca3984469bd9c82a7cfe3fbbf9f37f
guidovarela <guido.varela@gmail.com>
2023-09-20 20:41:11 -0300

    clase 11-12
    Bootstrap
    Git

---
+++ 13-JS1/js/2_javascript_externo.js
@@ -0,0 +1,43 @@
+
+
+
+//---------1. VARIABLES------------
+//------Numéricas, texto y booleanas
+/* Declarar una variable por nombre y mostrar en consola.
+Declarar una variable con var y mostrar en consola
+Declarar una variable con let y mostrar en consola
+Verificar el tipo de dato con typeof()*/
+
+
+
+
+
+//---2. SEPARAR variables y textos-----------
+// Dos formas con: + y ,
+
+
+
+
+//---3. Definir una constante--------
+// Crear una constante para PI y para IVA
+
+
+
+//------3. USO DEL prompt()---------
+/*Mostrar un mensaje solicitando datos,
+  crear una variable para almacenar un nombre, 
+  mostrar el nombre ingresado por consola*/
+
+
+
+
+/* Comentar lo anterior. 
+   Solicitar edad, almacenar en una 
+   variable y mostrar en consola la edad más 1.
+   Evaluar el resultado e incorporar solución 
+   para convertir el dato (parsear el dato parseInt,parseFloat, String()) */
+
+
+
+ 
+
---
+++ 13-JS1/js/3_operadores.js
@@ -0,0 +1,37 @@
+//------Oeradores Aritmáticos----
+// suma, resta multiplicación, división, resto y exponente
+// Crear dos variables numéricas, aplicar todas las
+// operaciones y mostrar resultados en consola.
+
+// Limpiar consola.
+
+
+//------Oeradores Incrementales y Asignación----
+// Inicializar una variable i en cero,
+// mostrar en consola,
+// luego incrementar (En uno y en más de uno), 
+// volver a mostrar en consola.
+// luego disminuir la cantidad (En uno y en más de uno).
+// mostrar en consola.
+
+
+//------Oeradores de comparación----------
+// Mayor,menor, igual,exactamente igual, diferentes,
+// mostrar en consola los resultados al utilizarlos
+
+
+
+//------Oeradores de cadena += ----------
+// Crear una variable llamada presentacion y almanecar  "Mi nombre es",
+// para luego concatenar con un nombre y mostrar en consola.
+
+
+
+//------Oeradores de Lógicos----------
+// AND: &&,
+// OR: ||, 
+// NOT: !
+// Mostrar en consola varias combinaciones lógicas,
+// Ejemplo con operaciones aritméticas y logícas:
+// crear una variable numérica,
+// mostrar en consola el resultado de dos operaciones incluidas en un log
