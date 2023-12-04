266b021d1b1c6204329c99d86506995f378c5b71
guidovarela <guido.varela@gmail.com>
2023-12-04 03:16:00 -0300

    crud

--- 30-crud-python/app/app.py
+++ 30-crud-python/app/app.py
@@ -1,44 +1,75 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, jsonify
+from flask_mysqldb import MySQL
+
+
+# importar logica de un arch externo
+from data_usuarios import data
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     saludar = "Estamos usando Flask!" 
     title="Inicio"
-
-    """  user = {
-        "nombre":"Guido",
-        "edad":42,
-        "mascotas":False
-    } """
-    user = ["Guido","Marcela","Pepe","Maria","Dario"]
-
-    usuarios= [
-        {
-        "nombre":"Guido",
-        "edad":42,
-        "mascotas":False
-        },
-        {
-        "nombre":"Federico",
-        "edad":23,
-        "mascotas":["Neron","rambo"]
-        },
-        {
-        "nombre":"Analia",
-        "edad":35,
-        "mascotas":["Toby"]
-        },
-    ]
-
-    return render_template("index.html",data=saludar,title=title, user=usuarios)
+    return render_template("index.html",data=saludar,title=title)
 
 @app.route('/contacto')
 def contacto():
     title2="Contacto"
     return render_template('contacto.html',title=title2)
+
+@app.route('/personas')
+def personasAll():
+    return render_template('personas.html', data=data, title="Personas")
+
+# rutas dinamicas
+@app.route('/personas/<int:id>')
+def persona(id):
+    print(data[id])
+    return render_template('persona.html', data=data[id], title="Personas")
+
+
+# MySQL
+
+host="localhost"
+
+# conexion
+app.config['MYSQL_HOST']=host
+app.config['MYSQL_USER']="root"
+app.config['MYSQL_PASSWORD']=""
+app.config['MYSQL_DB']="curso-python"
+
+conexion = MySQL(app)
+
+@app.route('/usuarios')
+def cargar_usuarios():
+    title="Usuarios"
+
+    # consulta datos
+    # objeto vacio
+    data = {}
+    try:
+        # cursor
+        cursor = conexion.connection.cursor()
+        # consulta
+        sql = "SELECT * FROM usuarios ORDER BY id ASC"
+        # ejecutar consulta
+        cursor.execute(sql)
+        usuarios = cursor.fetchall()
+        print(usuarios)
+        data['usuarios'] = usuarios
+        # data['mensaje'] = 'Exito'
+    except Exception as ex:
+        data['mensaje'] = 'Error...'
+
+    # return jsonify(data)
+    result = jsonify(data)
+    return render_template("usuarios.html",users=data,title=title)
+    # return render_template("usuarios.html",data=data)
+
+
 
 
 if __name__=="__main__":
